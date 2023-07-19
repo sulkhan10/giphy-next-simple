7927924902e43e7542151b606e4d1cd1b8446be0
sulkhan <sulkhangalangsakti@gmail.com>
2023-07-20 06:33:28 +0700

    feat : initialize pages and route

---
+++ src/app/ironman-giphy/page.js
@@ -0,0 +1,11 @@
+
+export default function IronManGiphy() {
+    return (
+        <div>
+        <h1>Search Giphy</h1>
+        <p>Search your giphy here:</p>
+        <input type="text" placeholder="Search your giphy here..." />
+        <button>Search</button>
+        </div>
+    )
+}
---
+++ src/app/search-giphy/page.js
@@ -0,0 +1,11 @@
+
+export default function SearchGiphy() {
+    return (
+        <div>
+        <h1>Search Giphy</h1>
+        <p>Search your giphy here:</p>
+        <input type="text" placeholder="Search your giphy here..." />
+        <button>Search</button>
+        </div>
+    )
+}
