7ae0687ae0fc39d6b1c4383dc37f47f6a25d70ef
sulkhan <sulkhangalangsakti@gmail.com>
2023-07-20 07:05:30 +0700

    feat : add home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import { Montserrat } from 'next/font/google'
+ 
+const montserrat = Montserrat({ subsets: ['latin'] })
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between p-24">
+    <div className={montserrat.className}>
+
+    <main className="flex h-screen flex-col items-center justify-around p-24">
+        <div>
+        <div class="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+          Welcome to Your Giphy</div>
+        </div>
+        <div className="w-1/2">
+
       <Image
         src={"https://media.giphy.com/media/3o6gbbuLW76jkt8vIc/giphy.gif"}
-        // layout={'responsive'}
+        layout={'responsive'}
         height={175}
         width={175}
         alt={`A cute animal!`}
         unoptimized={true}
       />
-
+</div>
       <div>
-        <h1>Welcome to Giphy App</h1>
-        <p>Please choose an option:</p>
-        <ul>
+        
+        <ul className="flex gap-16">
           <li>
             <Link href="/ironman-giphy">
-              <div>Iron Man Giphy</div>
+              <div className="text-xl text-red-400 font-bold tracking-wide hover:text-red-500 transition  duration-200 ease-in-out hover:animate-pulse">
+                Iron Man Giphy</div>
             </Link>
           </li>
           <li>
             <Link href="/search-giphy">
-              <div>Search Your Giphy</div>
+            <div className="text-xl text-red-400 font-bold tracking-wide hover:text-red-500 transition  duration-200 ease-in-out hover:animate-pulse">
+                Search Your Giphy</div>
             </Link>
           </li>
         </ul>
       </div>
     </main>
+    </div>
   );
 }
